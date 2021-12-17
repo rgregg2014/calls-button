4f039f38cf6c7fc1b3f779377ca32f25d3483b55
Rachel Gregg <rgregg2014@gmail.com>
2021-12-17 10:19:37 -0500

    added page elements, event listener

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,10 @@
 console.log("Scripts linked!");
+
+const button = document.querySelector("#callBtn");
+const nameHead = document.querySelector("#studentName");
+
+function callName() {
+  console.log("button clicked");
+}
+
+button.addEventListener("click", callName);
