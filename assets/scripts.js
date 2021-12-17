c6a71977f11e95b2e039d49db3392bc1bf1be96d
Rachel Gregg <rgregg2014@gmail.com>
2021-12-17 10:30:35 -0500

    it works now

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,9 +2,17 @@ console.log("Scripts linked!");
 
 const button = document.querySelector("#callBtn");
 const nameHead = document.querySelector("#studentName");
+const students = ["Chris", "Ami", "Hugo", "James"];
+
+function randomStudent() {
+  const randomIndex = Math.floor(Math.random() * students.length);
+  let randomStudent = students[randomIndex];
+  return randomStudent;
+}
 
 function callName() {
-  console.log("button clicked");
+  const student = randomStudent();
+  nameHead.textContent = student;
 }
 
 button.addEventListener("click", callName);
